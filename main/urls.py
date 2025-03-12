70bea62af6e7a35f4be16e60405e63dae33900d7
mfplinta <mfplinta@gmail.com>
2025-03-12 04:00:42 -0600

    Add robots.txt

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("projects/", views.projects, name="projects"),
     path("projects/with-tag/<str:tag>/", views.projects, name="projects-withtag"),
 ]
