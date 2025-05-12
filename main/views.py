b09b9f417012a93b68059bd1f5c4d6e7fc61221a
matheus <mfplinta@gmail.com>
2025-05-11 21:24:57 -0600

    Make it actually descendant

--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ def index(request):
     return render(request, 'main/index.html', {
         'skills': Skill.objects.all().order_by(F('title').asc()),
         'education': Education.objects.all().order_by(F('end_date').desc(nulls_first=True)),
-        'experiences': Experience.objects.all().order_by(F('end_date').desc(nulls_first=True), F('start_date')),
+        'experiences': Experience.objects.all().order_by(F('end_date').desc(nulls_first=True), F('start_date').desc()),
     })
 
 def projects(request, tag=None):
