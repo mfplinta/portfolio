2c6b26105f4c8f3cf27fc23641fc798ea22b9eb5
Matheus <mfplinta@gmail.com>
2025-05-28 01:19:37 -0600

    Temporarily enable 500 on debug=false

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from django.conf import settings
+import sys
+
+from django.conf import settings, urls
 from django.urls import path
 from django.conf.urls.static import static
+from django.views.debug import technical_500_response
 from django.views.generic.base import TemplateView
 
 from . import views
@@ -12,6 +14,8 @@ urlpatterns = [
     path("projects/", views.projects, name="projects"),
     path("projects/with-tag/<str:tag>/", views.projects, name="projects-withtag"),
 ]
+
+urls.handler500 = lambda req: technical_500_response(req, *sys.exc_info())
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
